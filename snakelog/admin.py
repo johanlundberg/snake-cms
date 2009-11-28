13ccd46425b8487f69084faac14e3968d3b1aab8
Johan Lundberg <lundberg.johan@gmail.com>
2009-11-28 23:38:43 +0100

    Commit before clean up of code.

--- snakelog/admin.py
+++ snakelog/admin.py
@@ -9,11 +9,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class EntryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    class Media:
+        js = ['/media/admin/tinymce/jscripts/tiny_mce/tiny_mce.js', '/media/admin/tinymce_setup/tinymce_setup_en.js',]
 
 class LinkAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
-# We have to unregister it if it registered
+# We have to unregister it if it was registered before
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Entry, EntryAdmin)
 admin.site.register(Link, LinkAdmin)
